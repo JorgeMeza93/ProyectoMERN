6996206aebbb830cc687c89c62839e9afdd4d997
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-21 01:01:32 -0500

    Generacion de un token único para cada registro

--- Backend/controllers/veterinarioController.js
+++ Backend/controllers/veterinarioController.js
@@ -2,6 +2,14 @@ import Veterinario from "../models/Veterinario.js";
 
 const registrar = async (req, res) =>{
     const {nombre, email, password} = req.body;
+    // Prevenir usuarios duplicados
+    const existeUsuario = await Veterinario.findOne( {email: email} );
+    if(existeUsuario){
+        console.log(existeUsuario);
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json( {msg: error.message} );
+    }
+    
     try {
         //Guardar un nuevo veterinario
         const veterinario = new Veterinario(req.body);
