294e237fdd0971369180cbddaf1930ae6a8ce85f
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-21 00:02:11 -0500

    Creacion del routing y conexion a la base de datos

---
+++ Backend/controllers/veterinarioController.js
@@ -0,0 +1,18 @@
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (req, res) =>{
+    const {nombre, email, password} = req.body;
+    try {
+        //Guardar un nuevo veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+        res.json( veterinarioGuardado );
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+const perfil = (req, res) =>{
+    res.json( {url: "Desde API/VETERINARIOS/perfil"} )
+};
+export {registrar, perfil};
--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import conectarDB from "./config/db.js";
 import dotenv from "dotenv";
+import veterinarioRoutes from "./routes/veterinarioRoutes.js"
 
 const app = express();
+app.use(express.json());
 dotenv.config();
 conectarDB();
-app.use("/", ( req, res ) => {
-    res.send("Hola Mundo");
-});
+app.use("/api/veterinarios", veterinarioRoutes );
 const PORT = process.env.PORT || 4000;
 app.listen(4000, () => {
     console.log(`Servidor funcionando en el puerto ${PORT}`);
